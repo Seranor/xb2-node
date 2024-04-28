631928cf0de87dccf6f9711e74743054960210ed
Seranor <liuzhijing1997@gmail.com>
2024-04-28 20:45:50 +0800

    learn2

--- src/main.js
+++ src/main.js
@@ -1,9 +1,53 @@
 const http = require('http');
-
+const { title } = require('process');
 const server = http.createServer((request, response) => {
-  response.write('hello ~');
+  const data = {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  response.writeHead(200, {
+    'Content-Type': 'application/json; charset=utf-8'
+  });
+
+  response.write(jsonData);
+
   response.end();
 });
+
+// const server = http.createServer((request, response) => {
+//   switch (request.url) {
+//     case '/':
+//       response.write('hello~ ');
+//       break;
+//     case '/posts':
+//       response.write('posts');
+//       break;
+//     case '/signup':
+//       response.write('signup');
+//       break;
+//     default:
+//       response.writeHead(404);
+//       response.write('404');
+//       break;
+//   }
+//   response.end()
+// })
+
+
+// const server = http.createServer((request, response) => {
+//   console.log(request.headers['user-agent']);
+
+//   response.writeHead(200, {
+//     'Content-Type': 'text/html',
+//   })
+
+//   response.write(`<input />`);
+//   response.end();
+// });
 
 server.listen(3000, () => {
   console.log('🚀 服务已启动！');
